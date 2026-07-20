c1b92ff6fec1ff462caca1d90d4df281b4c49a0f
AlVoloo <g.teoneg@gmail.com>
2026-07-20 21:51:38 +0200

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'La nostra lista nascita',
   description: 'Benvenuti nella nostra lista nascita',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎀</text></svg>',
+  },
 };
 
 export default function RootLayout({
